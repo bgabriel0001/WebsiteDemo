90412aea52de12fb3fe0af6e2467c5f68751d772
bgabr001 <bgabriel0001@icloud.com>
2025-07-15 16:28:34 -0400

    update

---
+++ script.js
@@ -0,0 +1,16 @@
+const headers = {
+  "main-header": "Brennen Gabriel",
+  "about": "About Me",
+  "projects": "Programming Projects",
+  "contact": "Contact"
+};
+
+for (const [id, text] of Object.entries(headers)) {
+  const el = document.getElementById(id);
+  if (el && el.tagName.startsWith('H')) {
+    el.textContent = text;
+  } else if (el) {
+    const heading = el.querySelector('h2');
+    if (heading) heading.textContent = text;
+  }
+}
